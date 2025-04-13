957959ab2f957b3f90de9b45f6d8ca972f91dfae
someTrickyCase <snowinboots18@gmail.com>
2025-04-13 17:42:26 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+"use strict";
+const { XMLParser, XMLBuilder } = require("fast-xml-parser");
+const fs = require("node:fs");
+
+const parser = new XMLParser();
+
+async function getXML(url) {
+    const xml = await fetch(url);
+    return await xml.text();
+}
+
+const xmlTeamplate = {
+    "?xml": "",
+    yml_catalog: {
+        shop: {
+            name: "to18_toAliexpress",
+            company: "to18",
+            url: "https://to18.ru",
+            currencies: {},
+            categories: {},
+            agency: "Troffi.ru",
+            email: "aa@troffi.ru",
+            offers: { offer: [] },
+        },
+    },
+};
+
+getXML(
+    "https://to18.ru/wp-content/uploads/woo-product-feed-pro/xml/uunu2Q58c6U9P1hIKr1ljyIFbAumrU7z.xml"
+).then((r) => {
+    const xmlThree = parser.parse(r);
+
+    xmlTeamplate.yml_catalog.shop.currencies = xmlThree.yml_catalog.shop.currencies;
+    xmlTeamplate.yml_catalog.shop.categories = xmlThree.yml_catalog.shop.categories;
+
+    const { offers } = xmlThree.yml_catalog.shop;
+    offers.offer.map((product) => {
+        const { weight, lenght, height, width } = product;
+        if (!weight || !lenght || !height | !width) return;
+        // recalculate price and add to new xmlObj
+        const oldPrice = +product.price.split(",")[0].replace(" ", "");
+        if (weight <= 2) product.price = oldPrice + 159;
+        else if (weight <= 5) product.price = oldPrice + 359;
+        else if (weight <= 20) product.price = oldPrice + 659;
+        else return;
+
+        xmlTeamplate.yml_catalog.shop.offers.offer.push({ ...product });
+    });
+
+    // write file
+    const builder = new XMLBuilder();
+    const xmlContent = builder.build(xmlTeamplate);
+    fs.writeFile("/feedOutputs/xmlFeedToAliexpress.xml", xmlContent, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("new xml writed successfully");
+        }
+    });
+});
